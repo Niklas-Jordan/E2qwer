42aa567eca2ae831165fab74a394903c456bcaf9
Niklas Jordan <Niklas.Jordan@hochschule-stralsund.de>
2019-12-10 16:16:58 +0100

    E 1.1 und E 1.2 abgeschlossen

---
+++ E1/src/LPS_Niklas_Jordan_SMIB/Main.java
@@ -0,0 +1,51 @@
+package LPS_Niklas_Jordan_SMIB;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            GridPane root = new GridPane();
+            Scene scene = new Scene(root,400,400);
+            TextField textField1 = new TextField();
+            textField1.setPromptText("Schreibe hier");
+            TextField textField2 = new TextField();
+            textField2.setPromptText("Schreibe hier");
+            final EventHandler<KeyEvent> keyEventHandler = new EventHandler<KeyEvent>() {
+                        public void handle(final KeyEvent keyEvent) {
+                            //Hier fehlt Ihr Code
+                            if (keyEvent.getCode() == KeyCode.F1) {
+                                textField2.setText("F1 gedrückt");
+                            }
+                            if (keyEvent.getCode() == KeyCode.F2) {
+                                textField2.setText("F2 gedrückt");
+                            }
+                            if (keyEvent.getCode() == KeyCode.F3) {
+                                textField2.setText("F3 gedrückt");
+                            }
+                            keyEvent.consume();
+                        }
+                    };
+            textField1.setOnKeyPressed(keyEventHandler);
+            textField2.setOnKeyPressed(keyEventHandler);
+            root.setConstraints(textField1, 0,0);
+            root.setConstraints(textField2, 0,1);
+            root.getChildren().addAll(textField1,textField2);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
