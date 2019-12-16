982a9ceb1502d1edc20f03415ee06fb269ceaf95
Niklas Jordan <Niklas.Jordan@hochschule-stralsund.de>
2019-12-16 12:14:58 +0100

    E2 angefangen, ERROR aufgrund von Module E2

---
+++ E2/src/module-info.java
@@ -0,0 +1,6 @@
+module E2 {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.web;
+    opens sample;
+}
---
+++ E2/src/sample/WikiBooks.java
@@ -0,0 +1,19 @@
+package sample;
+
+public class WikiBooks {
+    private String url;
+
+    public WikiBooks(String url) {
+        setUrl(url);
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        url = url.replace("", "_");
+        this.url = "https://de.wikibooks.org/wiki/Hauptseite"+url;
+    }
+}
+
