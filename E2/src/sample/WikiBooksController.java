06c060babdd68acbfd61b2d7b0dc44c5a3c8d14c
Niklas Jordan <Niklas.Jordan@hochschule-stralsund.de>
2019-12-18 17:55:51 +0100

    E2 funkt, aber nicht schick Enter Tasten Funktion fehlt noch

--- E2/src/sample/WikiBooksController.java
+++ E2/src/sample/WikiBooksController.java
@@ -1,8 +1,12 @@
 package sample;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
@@ -29,9 +33,29 @@ public class WikiBooksController {
 
     @FXML
     private void initialize() {
-        btnSearch.setOnAction((event) -> {
-            WikiBooks books = new WikiBooks(tfSearch.getText());
-            browser.getEngine().load(books.getUrl());
+        browser.getEngine().load("https://de.wikibooks.org/wiki/ ");
+
+        btnSearch.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                btnSearch.setOnAction((event) -> {
+                    WikiBooks books = new WikiBooks(tfSearch.getText());
+                    browser.getEngine().load(books.getUrl());
+                });
+            }
+        });
+        btnSearch.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                btnSearch.setOnAction((event) -> {
+                    if (keyEvent.getCode() == KeyCode.ENTER) {
+                        WikiBooks books = new WikiBooks(tfSearch.getText());
+                        browser.getEngine().load(books.getUrl());
+                    }
+                });
+            }
         });
     }
 }
+
+
