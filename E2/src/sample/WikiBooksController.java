53951c9a49d8093c50df1fd7b2e2d87bd4ff5405
Niklas Jordan <Niklas.Jordan@hochschule-stralsund.de>
2019-12-21 16:32:20 +0100

    E2.1 Enter Suchfunktion klappt und Methoden unterteilt

--- E2/src/sample/WikiBooksController.java
+++ E2/src/sample/WikiBooksController.java
@@ -1,12 +1,10 @@
 package sample;
 
-import javafx.event.EventHandler;
+import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
@@ -17,7 +15,7 @@ public class WikiBooksController {
     private AnchorPane anchorP;
 
     @FXML
-    private VBox vBoxBottom, vBoxTop;
+    private VBox vBoxBottom;
 
     @FXML
     private GridPane gridP;
@@ -35,26 +33,50 @@ public class WikiBooksController {
     private void initialize() {
         browser.getEngine().load("https://de.wikibooks.org/wiki/ ");
 
-        btnSearch.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                btnSearch.setOnAction((event) -> {
-                    WikiBooks books = new WikiBooks(tfSearch.getText());
-                    browser.getEngine().load(books.getUrl());
-                });
+        btnSearch.setOnAction((event) -> {
+            search();
+        });
+
+        tfSearch.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                search();
             }
         });
-        btnSearch.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent keyEvent) {
-                btnSearch.setOnAction((event) -> {
-                    if (keyEvent.getCode() == KeyCode.ENTER) {
-                        WikiBooks books = new WikiBooks(tfSearch.getText());
-                        browser.getEngine().load(books.getUrl());
+
+        browser.getEngine().getLoadWorker().stateProperty().addListener(((observableValue, oldValue, newValue) -> {
+            if (Worker.State.SUCCEEDED.equals(newValue)) {
+                if (browser.getEngine().getLocation().contains("https://de.wikibooks.org/wiki/ ")) {
+                    urlName = browser.getEngine().getLocation();
+                    urlName = urlName.replace("https://de.wikibooks.org/wiki/ ", "");
+                    tfSearch.setText(urlName);
+                    search();
+                    tfSearch.clear();
+                } else {
+                    urlName = browser.getEngine().getLocation();
+                    urlName = urlName.replace("https://de.wikibooks.org/w/index.php?search=", "");
+                    if (urlName.contains("&title")) {
+                        urlName = urlName.substring(0, urlName.indexOf("&title"));
                     }
-                });
+                    tfSearch.setText(urlName);
+                    search();
+                    tfSearch.clear();
+                }
             }
-        });
+        }));
+    }
+
+    private String urlName;
+
+    private void search() {
+        try {
+            String search = tfSearch.getText().trim().replace(" ", "_");
+
+            if (!urlName.equals(search)) {
+                browser.getEngine().load("https://de.wikibooks.org/wiki/" + search);
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Fehler beim Suchen!");
+        }
     }
 }
 
