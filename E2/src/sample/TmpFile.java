d7d1d470018905f9787029d28d353f7b9f880718
Niklas Jordan <Niklas.Jordan@hochschule-stralsund.de>
2019-12-22 14:21:46 +0100

    Klassen von Flo übernommen/angepasst

---
+++ E2/src/sample/TmpFile.java
@@ -0,0 +1,37 @@
+package sample;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+public class TmpFile {
+
+    /*
+     * createTmpFile
+     * Methode zum erstellen einer TmpFile
+     * @param _tmp
+     * @return
+     * @throws IOException
+     */
+    public File createTmpFile(ArrayList<String> _tmp) throws IOException {
+        File datei = File.createTempFile("temp", ".xml");
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(datei), "UTF8"));
+        datei.deleteOnExit();
+
+        for (String line : _tmp) {
+            writer.write(line);
+            writer.newLine();
+        }
+
+        writer.close();
+
+        BufferedReader br = new BufferedReader(new FileReader(datei));
+        br.close();
+        return datei;
+    }
+}
