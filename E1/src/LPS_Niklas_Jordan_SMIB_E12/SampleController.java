cface54e69a7ecaa7c8ef847b2803ce28e58f478
Niklas Jordan <Niklas.Jordan@hochschule-stralsund.de>
2019-12-31 15:32:19 +0100

    fertig und schick

--- E1/src/LPS_Niklas_Jordan_SMIB_E12/SampleController.java
+++ E1/src/LPS_Niklas_Jordan_SMIB_E12/SampleController.java
@@ -3,9 +3,20 @@ package LPS_Niklas_Jordan_SMIB_E12;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 
+/**
+ * The type Sample controller.
+ */
 public class SampleController {
+    /**
+     * The Hello world.
+     */
     public Label helloWorld;
 
+    /**
+     * Say hello world.
+     *
+     * @param actionEvent the action event
+     */
     public void sayHelloWorld(ActionEvent actionEvent) {
         helloWorld.setText("Hello World!");
     }
--- E1/src/LPS_Niklas_Jordan_SMIB_E12/sampleMain.java
+++ E1/src/LPS_Niklas_Jordan_SMIB_E12/sampleMain.java
@@ -6,17 +6,24 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * The type Sample main.
+ */
 public class sampleMain extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
 
-
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         launch(args);
     }
